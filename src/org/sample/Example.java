ea90d9091bb3f0d77ab7dd8b8aadabb88f82addb
kirubakaaran <karankiru1011@gmail.com>
2023-06-14 15:21:54 +0530

    add

--- src/org/sample/Example.java
+++ src/org/sample/Example.java
@@ -1,11 +1,12 @@
 package org.sample;
 
 public class Example {
+	
 	public void login(){
 		System.out.println("userName");
 		System.out.println("password");
 	}
 public static void main(String[] args) {
-
+System.out.println("");
 }
 }
