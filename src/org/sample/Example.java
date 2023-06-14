8917d49e43224765ecb3b9da043fa942995a4286
kirubakaaran <karankiru1011@gmail.com>
2023-06-14 15:28:26 +0530

    done

--- src/org/sample/Example.java
+++ src/org/sample/Example.java
@@ -5,6 +5,13 @@ public class Example {
 	public void login(){
 		System.out.println("userName");
 		System.out.println("password");
+	
+	}
+	public void user() {
+		System.out.println("user");
+	}
+	public void pass() {
+		System.out.println("pass");
 	}
 public static void main(String[] args) {
 System.out.println("");
